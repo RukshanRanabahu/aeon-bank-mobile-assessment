9856d0847a7e753a1f4d093f9519e3ad94749f6c
Rukshan Ranabahu <31883465+RukshanRanabahu@users.noreply.github.com>
2026-08-19 23:56:01 +0530

    test: fix Jest configuration and async rendering

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,10 +4,15 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
+
+jest.mock('../src/services/transactionService', () => ({
+  getTransactions: jest.fn().mockResolvedValue([]),
+}));
+
 import App from '../App';
 
 test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
+  await ReactTestRenderer.act(async () => {
     ReactTestRenderer.create(<App />);
   });
 });
